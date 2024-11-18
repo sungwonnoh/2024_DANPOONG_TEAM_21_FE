0b7696ae601fc6bcd35aedc6f389bd2d8fbe177d
sungwonnoh <jessica20730@naver.com>
2024-11-18 20:41:49 +0900

    feat: 폰트 추가

--- src/styles/Globalstyles.js
+++ src/styles/Globalstyles.js
@@ -35,6 +35,69 @@ const GlobalStyles = createGlobalStyle`
   input, ::placeholder, textarea {
     font-family: 'Pretendard';
   }
-`;
 
+  @font-face {
+    font-family: 'Pretendard';
+    font-weight: 900;
+    font-display: swap;
+    src: local('Pretendard Black'), url('/font/Pretendard-Black.ttf') format('truetype');
+  }
+
+  @font-face {
+    font-family: 'Pretendard';
+    font-weight: 800;
+    font-display: swap;
+    src: local('Pretendard ExtraBold'), url('/font/Pretendard-ExtraBold.ttf') format('truetype');
+  }
+
+  @font-face {
+    font-family: 'Pretendard';
+    font-weight: 700;
+    font-display: swap;
+    src: local('Pretendard Bold'), url('/font/Pretendard-Bold.ttf') format('truetype');
+  }
+
+  @font-face {
+    font-family: 'Pretendard';
+    font-weight: 600;
+    font-display: swap;
+    src: local('Pretendard SemiBold'), url('/font/Pretendard-SemiBold.ttf') format('truetype');
+  }
+
+  @font-face {
+    font-family: 'Pretendard';
+    font-weight: 500;
+    font-display: swap;
+    src: local('Pretendard Medium'), url('/font/Pretendard-Medium.ttf') format('truetype');
+  }
+
+  @font-face {
+    font-family: 'Pretendard';
+    font-weight: 400;
+    font-display: swap;
+    src: local('Pretendard Regular'), url('/font/Pretendard-Regular.ttf') format('truetype');
+  }
+
+  @font-face {
+    font-family: 'Pretendard';
+    font-weight: 300;
+    font-display: swap;
+    src: local('Pretendard Light'), url('/font/Pretendard-Light.ttf') format('truetype');
+  }
+
+  @font-face {
+    font-family: 'Pretendard';
+    font-weight: 200;
+    font-display: swap;
+    src: local('Pretendard ExtraLight'), url('/font/Pretendard-ExtraLight.ttf') format('truetype');
+  }
+
+  @font-face {
+    font-family: 'Pretendard';
+    font-weight: 100;
+    font-display: swap;
+    src: local('Pretendard Thin'), url('/font/Pretendard-Thin.ttf') format('truetype');
+  }
+
+`;
 export default GlobalStyles;

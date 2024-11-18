02d171297f2ad5f7715a3b58f81d75cb4e98cc80
sungwonnoh <jessica20730@naver.com>
2024-11-18 13:45:43 +0900

    feat: color palette

---
+++ src/styles/theme.js
@@ -0,0 +1,18 @@
+const theme = {
+  color: {
+    main: "#6736fa",
+    sub: "#9e79ff",
+    point: "#caff43",
+    gray1: "#7c8690",
+    gray2: "#dee2e6",
+    gray3: "#f1f3f5",
+    black: "#121212",
+    white: "#fcfcfc",
+    red: "#ff5323",
+    ui_1: "1a0a77",
+    ui_2: "3f2aa1",
+    ui_3: "#b0b7be",
+  },
+};
+
+export default theme;

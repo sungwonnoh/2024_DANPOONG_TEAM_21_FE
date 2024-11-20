9789b59d2305a4d28ac97453555a0a1bbf84df10
youngju6143 <youngju6143@naver.com>
2024-11-21 00:40:23 +0900

    feat: 메인화면 레이아웃 구현

--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -1,3 +1,53 @@
+import * as S from "./main.style";
+import title from "./assets/title.png";
+import Practice from "./assets/Practice.jsx";
+import Word from "./assets/Word.jsx";
+import Helper from "./assets/Helper.jsx";
+import Information from "./assets/Information.jsx";
+
 export default function Main() {
-  return <></>;
+  const datas = [
+    {
+      info1: "무인결제기",
+      info2: "실습하기",
+      component: <Practice />,
+    },
+    {
+      info1: "무인결제기",
+      info2: "단어장",
+      component: <Word />,
+    },
+    {
+      info1: "결제 화면",
+      info2: "설명 보기",
+      component: <Information />,
+    },
+    {
+      info1: "학습",
+      info2: "도우미",
+      component: <Helper />,
+    },
+  ];
+  return (
+    <S.Container>
+      <S.Title src={title} alt="title" />
+      <S.Info>
+        <p>설명 보기와 실습 등을 통해 학습을 시작하고,</p>
+        <p>무인결제기 사용법을 익혀보세요.</p>
+      </S.Info>
+      <S.GridContainer>
+        {datas.map((value, index) => {
+          return (
+            <S.GridBox key={index}>
+              <span>
+                <p>{value.info1}</p>
+                <p>{value.info2}</p>
+              </span>
+              <section>{value.component}</section>
+            </S.GridBox>
+          );
+        })}
+      </S.GridContainer>
+    </S.Container>
+  );
 }

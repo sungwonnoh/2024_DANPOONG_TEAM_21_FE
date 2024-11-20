e5bba9b25a14a1fbf23df88f1a2720394149dffc
youngju6143 <youngju6143@naver.com>
2024-11-21 01:12:50 +0900

    fix: 라우터 에러 수정

--- src/navigator/routes.jsx
+++ src/navigator/routes.jsx
@@ -1,10 +1,10 @@
 import { createBrowserRouter } from "react-router-dom";
-import Main from "../pages/main";
+import Main from "../pages/main/index";
 
 export const routes = createBrowserRouter([
   {
     path: "/",
-    component: Main,
+    element: <Main />,
   },
 ]);
 

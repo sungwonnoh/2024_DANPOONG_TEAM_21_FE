44f7d2c009761aef8ffec18e07154a0a41f743c7
sungwonnoh <jessica20730@naver.com>
2024-11-21 20:29:51 +0900

    refactor: 설명보기 메인화면 레이아웃 컴포넌트화

---
+++ src/components/startLayout.jsx
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+import theme from "../styles/theme";
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  //padding: 20px;
+  margin-top: 54px;
+  height: 90vh;
+  overflow: hidden;
+`;
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 0 8px;
+`;
+export const ShowTitle = styled.div`
+  font-size: 21px;
+  font-weight: 600;
+  color: ${theme.color.black};
+`;
+export const Contents = styled.div`
+  font-size: 15px;
+  font-weight: 500;
+  color: ${theme.color.gray1};
+  justify-content: center;
+`;
--- src/pages/main/description.jsx
+++ src/pages/main/description.jsx
@@ -1,36 +1,16 @@
 import styled from "styled-components";
+import {
+  Wrapper,
+  Container,
+  ShowTitle,
+  Contents,
+} from "../../components/startLayout";
 import choice_restaurant from "../../assets/images/choice_restaurant.svg";
 import choice_fastfood from "../../assets/images/choice_fastfood.svg";
 import choice_cafe from "../../assets/images/choice_cafe.svg";
-import theme from "../../styles/theme";
 import { Outlet, useNavigate } from "react-router-dom";
+import StartCard from "../../components/startCard";
 
-const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  justify-content: space-between;
-  //padding: 20px;
-  margin-top: 54px;
-  height: 90vh;
-  overflow: hidden;
-`;
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  padding: 0 8px;
-`;
-const ShowTitle = styled.div`
-  font-size: 21px;
-  font-weight: 600;
-  color: ${theme.color.black};
-`;
-const Contents = styled.div`
-  font-size: 15px;
-  font-weight: 500;
-  color: ${theme.color.gray1};
-  justify-content: center;
-`;
 const Choices = styled.div`
   display: flex;
   flex-direction: row;
@@ -38,38 +18,30 @@ const Choices = styled.div`
   align-items: center;
   flex-grow: 1;
 `;
-const Choice = styled.div`
-  width: 113px;
-  height: 200px;
-  background-color: ${theme.color.main};
-  border-radius: 8px;
-  margin: 4px;
-  padding: 8px;
-  position: relative;
-`;
-const ChoiceImage = styled.img`
-  position: absolute;
-  bottom: 8px;
-  right: 8px;
-  height: auto;
-`;
-const Title = styled.p`
-  font-size: 17px;
-  font-weight: 600;
-  color: ${theme.color.white};
-`;
 export default function Description() {
   const navigate = useNavigate();
 
-  const handleClick = (i) => {
-    if (i === "restaurant") {
-      navigate(`/description/${i}`);
-    } else if (i === "cafe") {
-      navigate(`/description/${i}`);
-    } else if (i === "fastfood") {
-      navigate(`/description/${i}`);
-    }
+  const handleClick = (type) => {
+    navigate(`/description/${type}`);
   };
+
+  const choices = [
+    {
+      title: ["음식점", "무인결제기"],
+      image: choice_restaurant,
+      onClick: () => handleClick("restaurant"),
+    },
+    {
+      title: ["카페", "무인결제기"],
+      image: choice_cafe,
+      onClick: () => handleClick("cafe"),
+    },
+    {
+      title: ["패스트푸드", "무인결제기"],
+      image: choice_fastfood,
+      onClick: () => handleClick("fastfood"),
+    },
+  ];
   return (
     <Wrapper>
       <Container>
@@ -78,21 +50,14 @@ export default function Description() {
         <Contents>해당하는 업종의 무인결제기 설명을 볼 수 있어요.</Contents>
       </Container>
       <Choices>
-        <Choice onClick={() => handleClick("restaurant")}>
-          <Title>음식점 </Title>
-          <Title>무인결제기</Title>
-          <ChoiceImage src={choice_restaurant} alt="업종선택_음식점" />
-        </Choice>
-        <Choice onClick={() => handleClick("cafe")}>
-          <Title>카페</Title>
-          <Title>무인결제기</Title>
-          <ChoiceImage src={choice_cafe} alt="업종선택_카페" />
-        </Choice>
-        <Choice onClick={() => handleClick("fastfood")}>
-          <Title>패스트푸드</Title>
-          <Title>무인결제기</Title>
-          <ChoiceImage src={choice_fastfood} alt="업종선택_패스트푸드" />
-        </Choice>
+        {choices.map((choice, index) => (
+          <StartCard
+            key={index}
+            title={choice.title}
+            image={choice.image}
+            onClick={choice.onClick}
+          />
+        ))}
       </Choices>
       <Outlet />
     </Wrapper>

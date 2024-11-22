8de5a03c1779ad5e1fd7982cf68058e3c008e6da
youngju6143 <youngju6143@naver.com>
2024-11-22 18:02:45 +0900

    feat: 시작 -> 완료 페이지 전환

--- src/pages/mainhelp/index.jsx
+++ src/pages/mainhelp/index.jsx
@@ -10,38 +10,40 @@ export default function MainHelp() {
   const handleStartClick = () => {
     setIsComplete(true);
   };
-
-  const handleCloseComplete = () => {
-    setIsComplete(false);
-  };
   return (
     <S.Container>
-      <S.Top>
-        <S.Title>
-          <h1>상단바를 내려</h1>
-          <h1>화면 녹화 기능을 켜주세요</h1>
-        </S.Title>
-        <S.Info>
-          <p>화면 녹화를 켠 후 ‘시작하기’ 버튼을 누르면</p>
-          <p>실습 페이지로 이동합니다.</p>
-        </S.Info>
-        <S.GridContainer>
-          <S.GridBox>
-            <S.GridIcon>
-              <VideoAndroid />
-            </S.GridIcon>
-            <p>안드로이드</p>
-          </S.GridBox>
-          <S.GridBox>
-            <S.GridIcon>
-              <VideoIos />
-            </S.GridIcon>
-            <p>아이폰</p>
-          </S.GridBox>
-        </S.GridContainer>
-      </S.Top>
+      {!isComplete ? (
+        <>
+          <S.Top>
+            <S.Title>
+              <h1>상단바를 내려</h1>
+              <h1>화면 녹화 기능을 켜주세요</h1>
+            </S.Title>
+            <S.Info>
+              <p>화면 녹화를 켠 후 ‘시작하기’ 버튼을 누르면</p>
+              <p>실습 페이지로 이동합니다.</p>
+            </S.Info>
+            <S.GridContainer>
+              <S.GridBox>
+                <S.GridIcon>
+                  <VideoAndroid />
+                </S.GridIcon>
+                <p>안드로이드</p>
+              </S.GridBox>
+              <S.GridBox>
+                <S.GridIcon>
+                  <VideoIos />
+                </S.GridIcon>
+                <p>아이폰</p>
+              </S.GridBox>
+            </S.GridContainer>
+          </S.Top>
 
-      <S.StartButton onClick={handleStartClick}>시작하기</S.StartButton>
+          <S.StartButton onClick={handleStartClick}>시작하기</S.StartButton>
+        </>
+      ) : (
+        <CompleteScreen />
+      )}
     </S.Container>
   );
 }
